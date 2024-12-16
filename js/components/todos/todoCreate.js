1223973744769eadbce0602a096553afb4d84602
Foisal Ahmed <foisal.cse@gmail.com>
2024-12-16 21:29:32 +0600

    design issue fixed

--- js/components/todos/todoCreate.js
+++ js/components/todos/todoCreate.js
@@ -1,8 +1,8 @@
 export default {
     template: `
         <form @submit.prevent="add">
-            <div class="border border-gray-600 text-black">
-                <input v-model="newTodo"  placeholder="Add new todos here..." class="p-2"/>
+            <div class="border border-gray-600 text-black flex">
+                <input v-model="newTodo"  placeholder="Add new todos here..." class="p-2 flex-grow"/>
                 <button class="p-2 border-l bg-white">Add</button>
             </div>
         </form>
