1fcab8e7fdee252ed02780173f534e683db12c18
Foisal Ahmed <foisal.cse@gmail.com>
2024-12-30 22:19:34 +0600

    define props and emits practice

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -5,13 +5,25 @@
 //   mixins: [flash]
 // }
 
+import { ref } from 'vue';
 import { useFlash } from '../composables/useFlash';
+import TabbableTextarea from '@/components/TabbableTextarea.vue';
 let { flash } = useFlash();
+let message = ref('default value');
+
+
 
 </script>
 
 <template>
   <main>
     <button @click="flash('alert from home')">Click me</button>
+
+    <br>
+    <br>
+    <br>
+
+    <TabbableTextarea v-model="message" style="width: 600px; height: 300px;"></TabbableTextarea>
+    
   </main>
 </template>
